2ed7adb43d39e66377987cbe76075981dac59150
Brandon Bjelland <brandon@atscale.run>
2023-07-27 10:26:46 -0700

    modify the flow to not default to our integration test project

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,7 +1,8 @@
 import os
+import subprocess
 
 import pytest
-from pytest_dependency import depends  # import the depends function
+from pytest_dependency import depends
 from testbook import testbook
 from google.cloud import storage
 import google.auth
@@ -21,9 +22,21 @@ def authenticate_to_gcp():
         raise ValueError("credentials.token is empty")
 
 
-def ensure_gcp_project():
-    project_id = os.environ.get("PROJECT_ID", "substratus-integration-tests")
-    return f"!gcloud config set project {project_id} -q"
+def ensure_gcp_project() -> str:
+    project_id = subprocess.run(
+        ["gcloud", "config", "get-value", "project"],
+        capture_output=True,
+        text=True,
+    ).stdout.strip()
+    if project_id and project_id != "(unset)":
+        return f"gcloud config set project {project_id} -q"
+
+    project_id = os.environ.get("PROJECT_ID")
+    if not project_id:
+        raise ValueError(
+            "Project ID is not set. Please set the PROJECT_ID environment variable."
+        )
+    return f"gcloud config set project {project_id} -q"
 
 
 @pytest.fixture(scope="session")
